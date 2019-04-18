741c05c2032895f71d54b1c15f380b3bd802c0a1
Paul Grote <paulgrote@Pauls-MacBook-Pro.local>
2019-04-18 08:41:39 -0500

    Create strings_practice.py

---
+++ strings_practice.py
@@ -0,0 +1,10 @@
+#strings practice
+first_name = "paul"
+last_name = "grote"
+full_name = first_name + " " + last_name
+print(full_name.title())
+print(full_name.lower())
+print(full_name.upper())
+
+message = "Hello " + full_name.title() + "!"
+print(message) 
