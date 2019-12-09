02e17a1e7e0349dc4d93410f2b23b900a588c37f
paulgrote <49725537+paulgrote@users.noreply.github.com>
2019-12-09 15:05:29 -0600

    edits

---
+++ favorite-cities.py
@@ -0,0 +1,11 @@
+# Has a function named favorite_city
+# favorite_city should have one parameter: name
+# Call favorite_city at least three times
+# The output should include: "One of my favorite cities is"
+
+def favorite_city(name):
+		print("One of my favorite cities is " + name)
+	
+favorite_city('London')
+favorite_city('Edinburgh')
+favorite_city('Lucerne')
